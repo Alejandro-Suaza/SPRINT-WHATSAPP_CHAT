b5a41fa93ca666f8e61c9c6f23211833fa96f3e1
Alejandro Suaza <jorgeivansuaza51@gmail.com>
2023-09-07 23:53:48 -0500

    Empezar con Users

---
+++ scripts/login.js
@@ -0,0 +1,39 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.getElementById("login-form");
+  const errorText = document.querySelector(".error-text");
+
+  form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const phoneNumber = document.getElementById("phoneNumber").value.trim();
+    const password = document.getElementById("password").value.trim();
+
+    if (phoneNumber === "" || password === "") {
+      errorText.textContent = "Todos los campos son obligatorios.";
+      errorText.style.display = "block";
+    } else {
+      try {
+        const response = await axios.get(`http://localhost:3000/users?phoneNumber=${phoneNumber}&password=${password}`);
+
+        if (response.status === 200) {
+          const users = response.data;
+          if (users.length === 1) {
+            // El inicio de sesión fue exitoso, almacenar información en localStorage o sessionStorage.
+            const user = users[0];
+            localStorage.setItem("loggedInUser", JSON.stringify(user));
+
+            // Redirige a la página de usuarios.
+            window.location.href = "../users.html";
+          } else {
+            errorText.textContent = "Credenciales incorrectas.";
+            errorText.style.display = "block";
+          }
+        }
+      } catch (error) {
+        console.error("Error al iniciar sesión:", error);
+        errorText.textContent = "Error al iniciar sesión.";
+        errorText.style.display = "block";
+      }
+    }
+  });
+});
--- scripts/signup.js
+++ scripts/signup.js
@@ -1,95 +1,56 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const form = document.getElementById("sign-form");
-    const errorText = document.querySelector(".error-text");
-  
-    // Función para obtener el último ID desde la base de datos, encontrar el último Id y en caso de no haber usuarios, empieza desde 0
-    async function getLastUserId() {
+  const form = document.getElementById("sign-form");
+  const errorText = document.querySelector(".error-text");
+
+  form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const name = document.getElementById("name").value.trim();
+    const lastName = document.getElementById("lastName").value.trim();
+    const phoneNumber = document.getElementById("phoneNumber").value.trim();
+    const password = document.getElementById("password").value.trim();
+    const img = document.getElementById("img").value.trim();
+
+    if (name === "" || lastName === "" || phoneNumber === "" || password === "" || img === "") {
+      errorText.textContent = "Todos los campos son obligatorios.";
+      errorText.style.display = "block";
+    } else {
       try {
-        const response = await axios.get("http://localhost:3000/users");
-  
+        const response = await axios.get(`http://localhost:3000/users?phoneNumber=${phoneNumber}`);
+
         if (response.status === 200) {
           const users = response.data;
           if (users.length > 0) {
-            const lastUser = users[users.length - 1];
-            return lastUser.id;
+            errorText.textContent = "Este número ya está registrado.";
+            errorText.style.display = "block";
           } else {
-            return 0;
-          }
-        }
-      } catch (error) {
-        console.error("Error al obtener usuarios:", error);
-        return -1; // Manejo de error, puedes establecer el valor deseado
-      }
-    }
-  
-    // Función para verificar si un número de teléfono ya está registrado
-    async function isPhoneNumberRegistered(phoneNumber) {
-      try {
-        const response = await axios.get("http://localhost:3000/users");
-        if (response.status === 200) {
-          const users = response.data;
-          return users.some((user) => user.phoneNumber === phoneNumber);
-        }
-      } catch (error) {
-        console.error("Error al obtener usuarios:", error);
-        return false; // Manejo de error, puedes establecer el valor deseado
-      }
-    }
-  
-    form.addEventListener("submit", async (e) => {
-      e.preventDefault();
-  
-      const name = document.getElementById("name").value.trim();
-      const lastName = document.getElementById("lastName").value.trim();
-      const phoneNumber = document.getElementById("phoneNumber").value.trim();
-      const password = document.getElementById("password").value.trim();
-      const img = document.getElementById("img").value.trim();
-  
-      if (name === "" || lastName === "" || phoneNumber === "" || password === "" || img === "") {
-        errorText.textContent = "Todos los campos son obligatorios.";
-        errorText.style.display = "block";
-      } else {
-        // Verifica si el número de teléfono ya está registrado
-        const phoneNumberRegistered = await isPhoneNumberRegistered(phoneNumber);
-  
-        if (phoneNumberRegistered) {
-          errorText.textContent = "Este número ya está registrado.";
-          errorText.style.display = "block";
-        } else {
-          // Obtén el último ID desde la base de datos
-          const lastUserId = await getLastUserId();
-  
-          if (lastUserId !== -1) {
-            // Incrementa el contador para obtener el próximo ID
+            const lastUserIdResponse = await axios.get("http://localhost:3000/users?_sort=id&_order=desc&_limit=1");
+            const lastUserId = lastUserIdResponse.data[0]?.id || 0;
             const nextUserId = lastUserId + 1;
-  
-            try {
-              const response = await axios.post("http://localhost:3000/users", {
-                id: nextUserId,
-                name,
-                lastName,
-                phoneNumber,
-                password,
-                img,
-              });
-  
-              // Muestra un mensaje de éxito
-              errorText.textContent = "Usuario creado exitosamente.";
-              errorText.style.color = "green"; // Color verde para indicar éxito
-  
-              // Redirige al usuario después de un breve retraso (por ejemplo, 2 segundos)
-              setTimeout(() => {
-                window.location.replace("../users.html");
-              }, 2000); // 2000 milisegundos = 2 segundos
-            } catch (error) {
-              console.error("Error al crear usuario:", error);
+
+            const createUserResponse = await axios.post("http://localhost:3000/users", {
+              id: nextUserId,
+              name,
+              lastName,
+              phoneNumber,
+              password,
+              img,
+            });
+
+            if (createUserResponse.status === 201) {
+              window.location.href = "../users.html";
+            } else {
+              console.error("Respuesta inesperada del servidor:", createUserResponse);
               errorText.textContent = "Error al crear usuario.";
+              errorText.style.display = "block";
             }
-          } else {
-            errorText.textContent = "Error al obtener el último ID.";
           }
         }
+      } catch (error) {
+        console.error("Error al obtener usuarios:", error);
+        errorText.textContent = "Error al crear usuario.";
+        errorText.style.display = "block";
       }
-    });
+    }
   });
-  
+});
